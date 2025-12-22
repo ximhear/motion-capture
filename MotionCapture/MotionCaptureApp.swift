e8655c5ba42551fe7567b4de3b0dcaffbd337b90
gzonelee <gzonelee@gmail.com>
2025-12-22 09:33:37 +0900

    Initial Commit

---
+++ MotionCapture/MotionCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionCaptureApp.swift
+//  MotionCapture
+//
+//  Created by gzonelee on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MotionCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
